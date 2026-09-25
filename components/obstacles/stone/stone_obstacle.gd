7400caca1b8caf85492e304e470aa26563cdd95f
Zidan-09 <samuel.profissional18@gmail.com>
2026-09-25 11:38:05 -0300

    feat(stone): implemented the same glass obstacle logic on second stage of stone obstacle

--- components/obstacles/stone/stone_obstacle.gd
+++ components/obstacles/stone/stone_obstacle.gd
@@ -1,10 +1,14 @@
 class_name StoneObstacle
 extends Obstacle
 
+@export_range(0.0, 1.0) var velocity_retain: float = 0.85
+
 @onready var crackedTexture = $CrackedStone
 @onready var polishedTexture = $PolishedStone
+@onready var hit_box: CollisionShape2D = $HitBox
 
-@onready var stoneLife := 2.0
+var stoneLife := 2
+var is_destroyed := false
 
 func _ready() -> void:
 	super._ready()
@@ -12,8 +16,23 @@ func _ready() -> void:
 	mat.friction = 0.0
 	mat.bounce = 1.0
 	physics_material_override = mat
-	
+
 	crackedTexture.visible = false
+
+func hit(bullet: RigidBody2D, impact_position: Vector2 = Vector2.INF) -> void:
+	if is_destroyed:
+		return
+	if bullet is GlassShard:
+		return
+	if stoneLife <= 1:
+		if not is_instance_valid(bullet):
+			return
+		is_destroyed = true
+		bullet.linear_velocity *= velocity_retain
+		hit_box.set_deferred("disabled", true)
+		queue_free()
+		return
+	super.hit(bullet, impact_position)
 
 func on_hit_started(_bullet: RigidBody2D) -> void:
 	if stoneLife == 2:
