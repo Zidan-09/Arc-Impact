aa7c0803b3296861f46fb48eed91704cc39aa1cd
Zidan-09 <samuel.profissional18@gmail.com>
2026-09-25 09:09:18 -0300

    fix(bounce): removed impulse on metal contact

--- components/obstacles/metal/metal_obstacle.gd
+++ components/obstacles/metal/metal_obstacle.gd
@@ -1,30 +1,14 @@
 class_name MetalObstacle
 extends Obstacle
 
-@export_range(0.0, 1.0) var velocity_retain: float = 0.95
-@export var min_ricochet_speed: float = 800.0
-
-var _incoming_velocity: Vector2
-
 
 func _ready() -> void:
 	super._ready()
+	var mat := PhysicsMaterial.new()
+	mat.friction = 0.0
+	mat.bounce = 1.0
+	physics_material_override = mat
 
 
-func on_hit_started(bullet: RigidBody2D) -> void:
-	_incoming_velocity = bullet.linear_velocity
-
-
-func on_hit_completed(bullet: RigidBody2D) -> void:
-	if not is_instance_valid(bullet):
-		return
-	var normal := bullet.global_position - global_position
-	if normal.length() < 1.0:
-		normal = -_incoming_velocity.normalized()
-	normal = normal.normalized()
-	if normal == Vector2.ZERO:
-		return
-	var reflected := _incoming_velocity.bounce(normal) * velocity_retain
-	if reflected.length() < min_ricochet_speed:
-		reflected = reflected.normalized() * min_ricochet_speed
-	bullet.linear_velocity = reflected
+func on_hit_completed(_bullet: RigidBody2D) -> void:
+	pass
